f5aeaa24d655f2ada89aa86e53cfd28256d98d80
jv-king <anshu7773019@gmail.com>
2024-12-24 14:08:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ API_HASH = os.environ.get("API_HASH", "1868dea7a187d9060a3c57be6a0f4182")
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002433338956"))
 FORCE_SUB_CHANNELS = os.environ.get("FORCE_SUB_CHANNELS", "-1002097394516,-1002271867183,-1002325985046 ").split(",")   # Multiple force sub channels
 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Soulreapers: jayvardhan@@@reapers.hvxag.mongodb.net/?retryWrites=true&w=majority&appName=Reapers")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Soulreapers:jayvardhan%40%40@reapers.hvxag.mongodb.net/?retryWrites=true&w=majority&appName=Reapers")
 DB_NAME = os.environ.get("DATABASE_NAME", "Reapers")
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", 10))
